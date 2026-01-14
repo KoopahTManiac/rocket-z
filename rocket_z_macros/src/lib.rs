4b7f1c8d7b1b6b1ef3a2b7e3147ab2f926c4cb2f
Zoran Ostojic <bata_zole@hotmail.com>
2026-01-14 23:30:23 +0100

    match rocket

--- rocket_z_macros/src/lib.rs
+++ rocket_z_macros/src/lib.rs
@@ -1,6 +1,7 @@
 use proc_macro::TokenStream;
 use quote::quote;
 use syn::{parse_macro_input, ItemFn, LitStr};
+use syn::{punctuated::Punctuated, Expr, ExprLit, Token};
 
 #[proc_macro_attribute]
 pub fn auto_route(args: TokenStream, input: TokenStream) -> TokenStream {
@@ -44,18 +45,43 @@ pub fn patch(args: TokenStream, input: TokenStream) -> TokenStream {
 }
 
 fn route_with_method(method: &str, args: TokenStream, input: TokenStream) -> TokenStream {
-    let mount = if args.is_empty() {
-        LitStr::new("/", proc_macro2::Span::call_site())
-    } else {
-        parse_macro_input!(args as LitStr)
+    let args_ts = proc_macro2::TokenStream::from(args.clone());
+    let mount = match parse_mount_from_args(args) {
+        Ok(mount) => mount,
+        Err(err) => return err.to_compile_error().into(),
     };
     let func = parse_macro_input!(input as ItemFn);
     let fn_name = &func.sig.ident;
     let method_ident = syn::Ident::new(method, proc_macro2::Span::call_site());
 
+    let route_attr = if args_ts.is_empty() {
+        quote! { #[rocket::#method_ident] }
+    } else {
+        quote! { #[rocket::#method_ident(#args_ts)] }
+    };
+
     TokenStream::from(quote! {
-        #[rocket::#method_ident(#mount)]
+        #route_attr
         #func
         rocket_z::register_routes!("/", rocket::routes![#fn_name]);
     })
+}
+
+fn parse_mount_from_args(args: TokenStream) -> Result<LitStr, syn::Error> {
+    if args.is_empty() {
+        return Ok(LitStr::new("/", proc_macro2::Span::call_site()));
+    }
+
+    let parsed = syn::parse::<Punctuated<Expr, Token![,]>>(args)?;
+    let Some(first) = parsed.first() else {
+        return Ok(LitStr::new("/", proc_macro2::Span::call_site()));
+    };
+
+    match first {
+        Expr::Lit(ExprLit { lit: syn::Lit::Str(lit), .. }) => Ok(lit.clone()),
+        _ => Err(syn::Error::new_spanned(
+            first,
+            "expected first argument to be a string literal route, e.g. \"/path\"",
+        )),
+    }
 }
